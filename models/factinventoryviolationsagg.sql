d7b4091d059b9c4a59f61d8a5afa97e5e9f6bd0b
david.hrncir@hashmapinc.com <david.hrncir@hashmapinc.com>
2022-08-18 00:01:52 -0500

    First

---
+++ models/factinventoryviolationsagg.sql
@@ -0,0 +1,11 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+select storekey, date_trunc('MONTH', datekey) as violation_month, 
+    count(*) as violations, count(distinct productkey) unique_products, 
+    avg(onhandquantity - safetystockquantity) as stock_diff 
+from {{ source('ft_raw', 'factinventory') }} 
+where safetystockquantity > onhandquantity group by storekey, violation_month
