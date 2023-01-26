bba0bccf4d08d4dbf9e360581f8078bc63b146e7
David Hrncir <david.hrncir@fivetran.com>
2023-01-26 08:09:55 -0600

    Changed to view. (#10)
    Co-authored-by: David Hrncir <david.hrncir@david.hrncir-X9YX0HKQKK>

--- models/agg_salesforce__opportunities.sql
+++ models/agg_salesforce__opportunities.sql
@@ -1,8 +1,3 @@
-{{
-  config(
-    materialized='table'
-  )
-}}
 
 select stage_name, count(*) as stage_count, round(avg(amount), 0) as stage_average 
 from {{source('hrncir_sf', 'opportunity')}} group by stage_name
